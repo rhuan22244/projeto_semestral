b3aef975299f6fa73e13d46d2d5bc647b5c8ce4c
NatallyThiel <145022387+NatallyThiel@users.noreply.github.com>
2025-08-08 11:29:20 -0300

    Salvando alterações

--- Animal.js
+++ Animal.js
@@ -1,17 +1,11 @@
 class Animal{
-    constructor(id, nome, especie, cliente){
+    constructor(id, nome, especie,){
         this.id = id;
         this.nome = nome;
         this.especie = especie;
-        this.cliente = cliente;
-    }
-    getNomeDoDono (){
-        return this.cliente.nome
     }
     
 }
-const cliente1 = new Cliente ("Rhuan Silva","6545687858",993712266);
 const testeAnimal = new Animal(1, "Kira", "Vira Lata", "Natally", cliente1);
 
 console.log(testeAnimal);
-testeAnimal.getNomeDoDono();
